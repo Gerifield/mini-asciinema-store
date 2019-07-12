b8b69ced746ba958cf71164dfaf90b9915ee3394
Gerifield <gergely.radics1@ibm.com>
2019-07-12 20:14:09 +0200

    feat: added HTTPS support

--- src/cmd/server/server.go
+++ src/cmd/server/server.go
@@ -11,13 +11,24 @@ import (
 
 func main()  {
 	listenAddr := flag.String("listenAddr", ":8080", "HTTP listening address")
+
 	uploadPath := flag.String("uploadPath", "uploads/", "Folder to store the uploaded files")
 	serverBaseURL := flag.String("serverBaseURL", "http://127.0.0.1:8080", "Base URL for the server")
+
+	https := flag.Bool("https", false, "HTTPS enable")
+	httpsCert := flag.String("httpsCert", "server.crt", "HTTPS cert")
+	httpsPrivateKey := flag.String("httpsPrivateKey", "server.key", "HTTPS private key")
 	flag.Parse()
 
 	srv := ministore.New(*uploadPath, *serverBaseURL)
 
-	err := http.ListenAndServe(*listenAddr, srv.Routes())
+	var err error
+
+	if *https {
+		err = http.ListenAndServeTLS(*listenAddr, *httpsCert, *httpsPrivateKey, srv.Routes())
+	} else {
+		err = http.ListenAndServe(*listenAddr, srv.Routes())
+	}
 	if err != nil {
 		log.Println(err)
 		os.Exit(1)
